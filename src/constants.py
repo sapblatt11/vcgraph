9e9ecd5c1c029fc9bf5196955f2c8f2a5d02cbb5
Aaron <asap@ipad.local>
2018-11-08 17:07:16 -0500

    Updated Constants

--- src/constants.py
+++ src/constants.py
@@ -1,7 +1,7 @@
 from datetime import datetime
 
-QUARTERS = [[1, 'Quarter 1', datetime(2017, 11, 3), ''], [2, 'Quarter 2', datetime(2018, 1, 19), ''],
-            [4, 'Quarter 3', datetime(2018, 3, 28), ''], [5, 'Quarter 4', datetime(2018, 6, 1), '']]
+QUARTERS = [[1, 'Quarter 1', datetime(2018, 11, 2), ''], [2, 'Quarter 2', datetime(2019, 1, 18), ''],
+            [3, 'Quarter 3', datetime(2019, 3, 29), ''], [4, 'Quarter 4', datetime(2019, 5, 31), '']]
 
 COURSE_NAMES = {
     "AP": [],
